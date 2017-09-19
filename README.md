36536a3eff5ff0dfeca196093987cada5017d538
Joe Greener <jgreener@hotmail.co.uk>
2017-09-19 17:16:56 +0100

    refactor, tests and tutorial

--- README.md
+++ README.md
@@ -4,14 +4,20 @@
 [![codecov.io](http://codecov.io/github/jgreener64/Bio3DView.jl/coverage.svg?branch=master)](http://codecov.io/github/jgreener64/Bio3DView.jl?branch=master)
 
 Bio3DView provides an interface to explore macromolecular 3D structures in [Jupyter](http://jupyter.org) notebooks.
-It uses [PyCall](https://github.com/JuliaPy/PyCall.jl) to call [py3Dmol](https://pypi.python.org/pypi/py3Dmol), a Python package that uses [3Dmol.js](http://3dmol.csb.pitt.edu) to render structures in the notebook.
+It uses [PyCall](https://github.com/JuliaPy/PyCall.jl) to call [py3Dmol](https://pypi.python.org/pypi/py3Dmol), a Python package that uses [3Dmol.js](http://3dmol.csb.pitt.edu) [1] to render structures in the notebook.
+
+[1] Nicholas Rego and David Koes,
+3Dmol.js: molecular visualization with WebGL,
+Bioinformatics (2015) 31(8): 1322-1324 - [link](http://doi.org/10.1093/bioinformatics/btu829)
 
 This project is work in progress.
 Contributions and bug reports are welcome.
 
 ## Installation
 
-Julia v0.6 is required. Install Bio3DView from the Julia REPL:
+Julia v0.6 is required.
+To use Julia in the Jupyter notebook, [IJulia](https://github.com/JuliaLang/IJulia.jl) also needs to be installed.
+Install Bio3DView from the Julia REPL:
 
 ```julia
 julia> Pkg.add("Bio3DView")
--- src/Bio3DView.jl
+++ src/Bio3DView.jl
@@ -2,32 +2,82 @@ module Bio3DView
 
 # Add precompile
 
-export view3D
+export
+    defaultstyle,
+    setstyle!,
+    addmodel!,
+    view3D,
+    viewfile,
+    viewpdb
 
 using PyCall
 using BioStructures
 
 @pyimport py3Dmol
 
-# Check IJulia with
-# isdefined(Main, :IJulia) && Main.IJulia.inited
+const defaultstyle = Dict("cartoon"=> Dict("color"=> "spectrum"))
 
-defaultstyle = Dict("cartoon"=> Dict("color"=> "spectrum"))
-
-# Name chosen to avoid collision with Base.view
-function view3D(s::AbstractString; style::Dict=defaultstyle)
-    v = py3Dmol.view()
-    v[:addModel](s, "pdb")
+# Can this actually be a string?
+function setstyle!(v::PyObject, style::Dict)
     v[:setStyle](style)
-    v[:zoomTo]()
     return v
 end
 
-function view3D(e::StructuralElementOrList, atom_selectors::Function...; kwargs...)
+function addmodel!(v::PyObject,
+                s::AbstractString,
+                format::AbstractString;
+                style::Dict=defaultstyle)
+    v[:addModel](s, format)
+    v[:zoomTo]()
+    setstyle!(v, style)
+    return v
+end
+
+function addmodel!(v::PyObject,
+                e::StructuralElementOrList,
+                atom_selectors::Function...;
+                kwargs...)
     io = IOBuffer()
     writepdb(io, e, atom_selectors...)
     s = String(io)
-    return view3D(s; kwargs...)
+    addmodel!(v, s, "pdb"; kwargs...)
+end
+
+view3D() = py3Dmol.view()
+
+function view3D(s::AbstractString,
+                format::AbstractString;
+                style::Dict=defaultstyle)
+    v = view3D()
+    addmodel!(v, s, format; style=style)
+    return v
+end
+
+function view3D(e::StructuralElementOrList,
+                atom_selectors::Function...;
+                kwargs...)
+    io = IOBuffer()
+    writepdb(io, e, atom_selectors...)
+    s = String(io)
+    return view3D(s, "pdb"; kwargs...)
+end
+
+function viewfile(f::AbstractString,
+                format::AbstractString;
+                kwargs...)
+    in_file = open(f)
+    s = readstring(in_file)
+    close(in_file)
+    return view3D(s, format; kwargs...)
+end
+
+function viewpdb(p::AbstractString; style::Dict=defaultstyle)
+    if !ismatch(r"^[a-zA-Z0-9]{4}$", p)
+        throw(ArgumentError("Not a valid PDB ID: \"$p\""))
+    end
+    v = py3Dmol.view(query="pdb:$(lowercase(p))")
+    setstyle!(v, style)
+    return v
 end
 
 end
