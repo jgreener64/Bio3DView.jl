284179125f5a6a600f3ee7e876ab6a7660d5d31d
Joe Greener <jgreener@hotmail.co.uk>
2018-08-11 16:27:50 +0100

    Style type to style output

--- src/Bio3DView.jl
+++ src/Bio3DView.jl
@@ -1,6 +1,7 @@
 module Bio3DView
 
 export
+    Style,
     defaultstyle,
     viewfile,
     viewstring,
@@ -15,8 +16,20 @@ isijulia() = isdefined(Main, :IJulia) && Main.IJulia.inited
 libpath = normpath(@__DIR__, "..", "js", "3Dmol-min.js")
 js3dmol = readstring(libpath)
 
-"`Dict` of default styles for molecular visualisation."
-const defaultstyle = Dict("cartoon"=> Dict("color"=> "spectrum"))
+"""
+A style for a molecular visualisation.
+Examples are `Style("cartoon")` and
+`Style("cartoon", Dict("color"=> "spectrum", "ribbon"=> true, "thickness"=> 1.0))`.
+"""
+struct Style
+    styletype::String
+    options::Dict{String, Any}
+end
+
+Style(s::AbstractString) = Style(s, Dict())
+
+"Default `Style` for molecular visualisation."
+const defaultstyle = Style("cartoon", Dict("color"=> "spectrum"))
 
 # Counter for data elements so they can be named individually
 elementcount = 0
@@ -26,11 +39,11 @@ View a molecular structure from a file or URL.
 Displays in a popup window, or in the output cell for an IJulia notebook.
 Arguments are the filepath/URL and the format ("pdb", "sdf", "xyz", "mol2", or
 "cube").
-The optional keyword argument `style` is a `Dict` of style options.
+The optional keyword argument `style` is a `Style`.
 """
 function viewfile(f::AbstractString,
                 format::AbstractString;
-                style::Dict=defaultstyle)
+                style::Style=defaultstyle)
     if isijulia()
         return view("data-type='$format'", readstring(f); style=style)
     else
@@ -43,11 +56,11 @@ View a molecular structure contained in a string.
 Displays in a popup window, or in the output cell for an IJulia notebook.
 Arguments are the molecule string and the format ("pdb", "sdf", "xyz", "mol2",
 or "cube").
-The optional keyword argument `style` is a `Dict` of style options.
+The optional keyword argument `style` is a `Style`.
 """
 function viewstring(s::AbstractString,
                 format::AbstractString;
-                style::Dict=defaultstyle)
+                style::Style=defaultstyle)
     return view("data-type='$format'", s; style=style)
 end
 
@@ -56,11 +69,11 @@ View a structural element from BioStructures.jl.
 Displays in a popup window, or in the output cell for an IJulia notebook.
 Arguments are a `StructuralElementOrList` and zero or more functions to act as
 atom selectors - see BioStructures.jl documentation for more.
-The optional keyword argument `style` is a `Dict` of style options.
+The optional keyword argument `style` is a `Style`.
 """
 function viewstruc(e::StructuralElementOrList,
                 atom_selectors::Function...;
-                style::Dict=defaultstyle)
+                style::Style=defaultstyle)
     io = IOBuffer()
     writepdb(io, e, atom_selectors...)
     return view("data-type='pdb'", String(io); style=style)
@@ -70,9 +83,9 @@ end
 View a structure from the Protein Data Bank (PDB).
 Displays in a popup window, or in the output cell for an IJulia notebook.
 Argument is the four letter PDB ID, e.g. "1AKE".
-The optional keyword argument `style` is a `Dict` of style options.
+The optional keyword argument `style` is a `Style`.
 """
-function viewpdb(p::AbstractString; style::Dict=defaultstyle)
+function viewpdb(p::AbstractString; style::Style=defaultstyle)
     if !ismatch(r"^[a-zA-Z0-9]{4}$", p)
         throw(ArgumentError("Not a valid PDB ID: \"$p\""))
     end
@@ -82,7 +95,7 @@ end
 # Generate HTML to view a molecule
 function view(tagstr::AbstractString,
                 datastr::AbstractString="";
-                style::Dict=defaultstyle)
+                style::Style=defaultstyle)
     if length(datastr) > 0
         global elementcount
         elementcount += 1
@@ -107,9 +120,18 @@ function view(tagstr::AbstractString,
     end
 end
 
-# Convert dictionary to a style string
-function stylestring(style::Dict)
-    return ""
+# Convert Style instance to a style string
+# String format is "cartoon:color=red,ribbon=true"
+function stylestring(style::Style)
+    s = style.styletype
+    if length(style.options) > 0
+        s *= ":"
+        for k in keys(style.options)
+            s *= "$k=$(string(style.options[k])),"
+        end
+        s = s[1:end - 1]
+    end
+    return s
 end
 
 end
