1b71254b1faaeb43ace4d0f95ff2628b69fcb097
Joe Greener <jgreener@hotmail.co.uk>
2018-10-04 16:22:36 +0100

    readme and tutorial update, variable renames

--- README.md
+++ README.md
@@ -7,30 +7,27 @@
 Bio3DView.jl provides a viewer for macromolecular 3D structures in [Julia](https://julialang.org).
 It is a wrapper round the excellent [3Dmol.js](http://3dmol.csb.pitt.edu) package [1].
 When used from the REPL or a file, the viewer shows in a popup using [Blink.jl](https://github.com/JunoLab/Blink.jl).
-When used from [IJulia](https://github.com/JuliaLang/IJulia.jl) running in a [Jupyter](http://jupyter.org) notebook, the viewer shows in the output cell.
+When used from [IJulia](https://github.com/JuliaLang/IJulia.jl) running in a [Jupyter](http://jupyter.org) notebook or JupyterLab, the viewer shows in the output cell.
 
 [1] Nicholas Rego and David Koes,
 3Dmol.js: molecular visualization with WebGL,
 Bioinformatics (2015) 31(8): 1322-1324 - [link](http://doi.org/10.1093/bioinformatics/btu829)
 
-This project is in development.
-Current status: main functions working, at least on Windows. Some bugs, including the REPL popup on some systems.
-Development and stabilisation may have to wait until Julia v1.0 is out.
 Contributions and bug reports are welcome.
 
 ## Installation
 
-Julia v0.6 is required.
+Julia v0.7 or later is required.
 Install Bio3DView from the Julia REPL:
 
 ```julia
-julia> Pkg.clone("https://github.com/jgreener64/Bio3DView.jl.git")
+using Pkg
+Pkg.clone("https://github.com/jgreener64/Bio3DView.jl.git")
 ```
 
 You may need to run `using Blink; Blink.AtomShell.install()` if you have not set up Blink.jl before.
-If you want to use Bio3DView.jl in a notebook (optional), [IJulia](https://github.com/JuliaLang/IJulia.jl) also needs to be installed.
+If you want to use Bio3DView.jl with Jupyter (optional), [IJulia](https://github.com/JuliaLang/IJulia.jl) also needs to be installed.
 
 ## Usage
 
 See the [tutorial notebook](http://nbviewer.jupyter.org/github/jgreener64/Bio3DView.jl/blob/master/examples/tutorial.ipynb).
-This is out of date (PyCall.jl is not longer used) and will be updated.
--- src/Bio3DView.jl
+++ src/Bio3DView.jl
@@ -13,19 +13,22 @@ using BioStructures
 
 isijulia() = isdefined(Main, :IJulia) && Main.IJulia.inited
 
-libpath = normpath(@__DIR__, "..", "js")
-path_3dmol = joinpath(libpath, "3Dmol-nojquery-min.js")
-path_jquery = joinpath(libpath, "jquery-3.3.1.min.js")
+path_lib = normpath(@__DIR__, "..", "js")
+path_3dmol = joinpath(path_lib, "3Dmol-nojquery-min.js")
+path_jquery = joinpath(path_lib, "jquery-3.3.1.min.js")
 js_3dmol = read(path_3dmol, String)
 js_jquery = read(path_jquery, String)
 
 """
-A style for a molecular visualisation.
+    Style(style_type)
+    Style(style_type, options)
+
+A style for a molecular visualisation, with an optional `Dict` of options.
 Examples are `Style("cartoon")` and
 `Style("cartoon", Dict("color"=> "spectrum", "ribbon"=> true, "thickness"=> 1.0))`.
 """
 struct Style
-    styletype::String
+    style_type::String
     options::Dict{String, Any}
 end
 
@@ -35,9 +38,12 @@ Style(s::AbstractString) = Style(s, Dict())
 const defaultstyle = Style("cartoon", Dict("color"=> "spectrum"))
 
 # Counter for data elements so they can be named individually
-elementcount = 0
+element_count = 0
 
 """
+    viewfile(file, format)
+    viewfile(url, format)
+
 View a molecular structure from a file or URL.
 Displays in a popup window, or in the output cell for an IJulia notebook.
 Arguments are the filepath/URL and the format ("pdb", "sdf", "xyz", "mol2", or
@@ -55,6 +61,8 @@ function viewfile(f::AbstractString,
 end
 
 """
+    viewstring(str, format)
+
 View a molecular structure contained in a string.
 Displays in a popup window, or in the output cell for an IJulia notebook.
 Arguments are the molecule string and the format ("pdb", "sdf", "xyz", "mol2",
@@ -68,6 +76,9 @@ function viewstring(s::AbstractString,
 end
 
 """
+    viewstruc(struc)
+    viewstruc(struc, atom_selectors...)
+
 View a structural element from BioStructures.jl.
 Displays in a popup window, or in the output cell for an IJulia notebook.
 Arguments are a `StructuralElementOrList` and zero or more functions to act as
@@ -79,10 +90,12 @@ function viewstruc(e::StructuralElementOrList,
                 style::Style=defaultstyle)
     io = IOBuffer()
     writepdb(io, e, atom_selectors...)
-    return view("data-type='pdb'", String(io); style=style)
+    return view("data-type='pdb'", String(take!(io)); style=style)
 end
 
 """
+    viewpdb(pdbid)
+
 View a structure from the Protein Data Bank (PDB).
 Displays in a popup window, or in the output cell for an IJulia notebook.
 Argument is the four letter PDB ID, e.g. "1AKE".
@@ -96,25 +109,25 @@ function viewpdb(p::AbstractString; style::Style=defaultstyle)
 end
 
 # Generate HTML to view a molecule
-function view(tagstr::AbstractString,
-                datastr::AbstractString="";
+function view(tag_str::AbstractString,
+                data_str::AbstractString="";
                 style::Style=defaultstyle)
-    if length(datastr) > 0
-        global elementcount
-        elementcount += 1
-        dataelement = "3dmol_data_$elementcount"
-        datadiv = "<textarea style='display:none;' id='$dataelement'>$datastr</textarea>"
-        tagstr *= " data-element='$dataelement'"
+    if length(data_str) > 0
+        global element_count
+        element_count += 1
+        data_element = "3dmol_data_$element_count"
+        data_div = "<textarea style='display:none;' id='$data_element'>$data_str</textarea>"
+        tag_str *= " data-element='$data_element'"
     else
-        datadiv = ""
+        data_div = ""
     end
-    divstr = "<div style='height: 540px; width: 540px;' " *
-        "class='viewer_3Dmoljs' $tagstr " *
+    div_str = "<div style='height: 540px; width: 540px;' " *
+        "class='viewer_3Dmoljs' $tag_str " *
         "data-backgroundcolor='0xffffff' " *
         "data-style='$(stylestring(style))'></div>"
     if isijulia()
         return HTML("<script type='text/javascript'>$js_jquery</script>" *
-            "<script type='text/javascript'>$js_3dmol</script>$datadiv$divstr")
+            "<script type='text/javascript'>$js_3dmol</script>$data_div$div_str")
     else
         w = Window()
         title(w, "Bio3DView")
@@ -126,7 +139,7 @@ function view(tagstr::AbstractString,
         end
         loadhtml(w, "<script>window.\$ = window.jQuery = require('$req_path');</script>" *
             "<script src='$path_jquery'></script><script src='$path_3dmol'>" *
-            "</script>$datadiv$divstr")
+            "</script>$data_div$div_str")
         return w
     end
 end
@@ -134,12 +147,13 @@ end
 # Convert Style instance to a style string
 # String format is "cartoon:color=red,ribbon=true"
 function stylestring(style::Style)
-    s = style.styletype
+    s = style.style_type
     if length(style.options) > 0
         s *= ":"
         for k in keys(style.options)
             s *= "$k=$(string(style.options[k])),"
         end
+        # Strip trailing comma
         s = s[1:end - 1]
     end
     return s
